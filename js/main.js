317a11f45f41a7ca981f850f445521e2fca1f107
Den Kuznetsov <webrote@yandex.ru>
2018-01-30 17:55:38 +0300

    30.01.18; 17:55

---
+++ js/main.js
@@ -0,0 +1,16 @@
+'use strict';
+/*=============
+ * Site scripts
+ ==============*/
+var windowW = $(window).width(),
+	openMenu = false;
+
+var site = {
+
+};
+site.init = function () {
+	
+};
+$(function () {
+	site.init();
+});
